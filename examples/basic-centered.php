5503983280b30d2da067b3a4c3e48385c80c84f4
Aydin Hassan <aydin@hotmail.co.uk>
2018-05-07 11:51:35 +0200

    Add example for centering menu

---
+++ examples/basic-centered.php
@@ -0,0 +1,22 @@
+<?php
+
+use PhpSchool\CliMenu\CliMenu;
+use PhpSchool\CliMenu\CliMenuBuilder;
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+$itemCallable = function (CliMenu $menu) {
+    echo $menu->getSelectedItem()->getText();
+};
+
+$menu = (new CliMenuBuilder)
+    ->setTitle('Basic CLI Menu')
+    ->addItem('First Item', $itemCallable)
+    ->addItem('Second Item', $itemCallable)
+    ->addItem('Third Item', $itemCallable)
+    ->addLineBreak('-')
+    ->setWidth(70)
+    ->setMargin(-1)
+    ->build();
+
+$menu->open();
