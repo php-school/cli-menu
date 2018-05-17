4648eb1c0ac9662a968f49bfcb6092960f96c09b
Aydin Hassan <aydin@hotmail.co.uk>
2018-05-17 13:32:58 +0200

    Add drawing example

---
+++ examples/draw.php
@@ -0,0 +1,64 @@
+<?php
+
+use PhpSchool\CliMenu\Action\ExitAction;
+use PhpSchool\CliMenu\CliMenu;
+use PhpSchool\CliMenu\Builder\CliMenuBuilder;
+use PhpSchool\CliMenu\MenuItem\SplitItem;
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+$cols = 60;
+$rows = 20;
+
+$paint = function (CliMenu $menu) {
+    $item = $menu->getSelectedItem();
+    if ($item->getText() === ' ') {
+        $item->setText('█');
+    } else {
+        $item->setText(' ');
+    }
+};
+
+$clear = function (CliMenu $menu) {
+    foreach ($menu->getItems() as $item) {
+        if ($item instanceof SplitItem && $item->canSelect()) {
+            foreach ($item->getItems() as $cell) {
+                if ($cell->canSelect() && $cell->getText() !== ' ') {
+                    $cell->setText(' ');
+                }
+            }
+        }
+    }
+    $menu->redraw(false);
+};
+
+$builder = (new CliMenuBuilder)
+    ->disableDefaultItems()
+    ->setWidth($cols + 10)
+    ->setBorder(0)
+    ->setMargin(2)
+    ->setPadding(2, 5)
+    ->setSelectedMarker('')
+    ->setUnselectedMarker('')
+    ->addAsciiArt('Draw your own art !')
+    ->addLineBreak();
+
+for ($i = 0; $i < $rows; $i++) {
+    $builder->addSplitItem(function() use ($cols, $paint) {
+        $this->setGutter(0);
+        for ($j = 0; $j < $cols; $j++) {
+            $this->addItem(' ', $paint);
+        }
+    });
+}
+
+$builder->addSplitItem(function() {
+    $this->addStaticItem('Enter: Toggle draw')
+        ->addStaticItem('C: Clear screen')
+        ->addStaticItem('X: Exit');
+});
+
+$menu = $builder->build();
+
+$menu->addCustomControlMappings(['C' => $clear, 'X' => new ExitAction]);
+$menu->open();
