895e74a8b2d3419e02393ebeb76d6faa13abb2f1
Juan Treminio <jtreminio@gmail.com>
2019-12-19 12:53:39 -0600

    Removes extra code

--- examples/split-checkbox-item.php
+++ examples/split-checkbox-item.php
@@ -6,10 +6,6 @@ use PhpSchool\CliMenu\Builder\CliMenuBuilder;
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 $itemCallable = function (CliMenu $menu) {
     echo $menu->getSelectedItem()->getText();
 };
