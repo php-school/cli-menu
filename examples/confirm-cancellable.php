71be47613ce8c3545ee61f7ca192fff6b11739a3
Joseph Sugamele <jsugamele@gmail.com>
2021-10-23 15:46:30 -0400

    Added example

---
+++ examples/confirm-cancellable.php
@@ -0,0 +1,29 @@
+<?php
+
+use PhpSchool\CliMenu\CliMenu;
+use PhpSchool\CliMenu\Builder\CliMenuBuilder;
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+$itemCallable = function (CliMenu $menu) {
+    $continue = $menu->confirm('PHP School FTW!', null, true)
+        ->display('OK', 'Cancel');
+
+    if ($continue) {
+        // Something Destructive
+        echo "OK";
+    } else {
+        // Do nothing
+        echo "Cancel";
+    }
+};
+
+$menu = (new CliMenuBuilder)
+    ->setTitle('Basic CLI Menu')
+    ->addItem('First Item', $itemCallable)
+    ->addItem('Second Item', $itemCallable)
+    ->addItem('Third Item', $itemCallable)
+    ->addLineBreak('-')
+    ->build();
+
+$menu->open();
